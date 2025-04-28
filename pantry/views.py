b85f642d40c5f967d3759d74a7edd0c3ca3ba91d
Aisyah Hawari <aisyahawari88@gmail.com>
2025-04-29 07:33:01 +0800

    Pantry: changed HTML and modified templates to display ingredients from database.

--- pantry/views.py
+++ pantry/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from . models import Ingredient
+from . models import PantryItem
 
 def ingredient_list(request):
-    return render(request, 'pantry/ingredient_list.html', {})
+    ingredients=Ingredient.objects.all() 
+    return render(request, 'pantry/ingredient_list.html', {'ingredients': ingredients})
 def pantryItem_list(request):
-    return render(request, 'pantry/pantryItem_list.html', {})
+    pantryItem= PantryItem.objects.order_by('expiry_date')
+    return render(request, 'pantry/pantryItem_list.html', {'pantryItem': pantryItem})
 # Create your views here.
