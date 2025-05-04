81c540bb651763140c71b7f0932795a7009f009e
Aisyah Hawari <aisyahawari88@gmail.com>
2025-05-05 02:02:24 +0800

    Pantry: Added form to add new pantry item

--- pantry/urls.py
+++ pantry/urls.py
@@ -4,4 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.ingredient_list, name='ingredient_list'),
     path('<str:category>/', views.ingredient_list, name='ingredient_list_by_category'),
+    # Form view
+    path('pantry/item/new/', views.pantry_item_new, name='pantry_item_new_form'),
+
+# Display item view
+    path('pantry/item/<str:name>/<int:quantity>/<str:unit>/<str:location>/<str:expiry>/', views.pantry_item_detail, name='pantry_item_detail'),
+    # urls.py
+    path('pantry/item/new/', views.pantry_item_new, name='pantry_item_new_form'),
+
 ]
--- pantry/views.py
+++ pantry/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from . models import Ingredient
 from . models import PantryItem
+from .forms import PantryForm
+from django.shortcuts import redirect
 
 def base(request):
     return render(request, 'pantry/base.html')
@@ -12,5 +14,38 @@ def ingredient_list(request, category=None):
 
 
     return render(request, 'pantry/ingredient_list.html', {'pantry_items': pantry_items,  'active_category': category or 'all',})
+# Handles form submission
+def pantry_item_new(request):
+    if request.method == "POST":
+        form = PantryForm(request.POST)
+        if form.is_valid():
+            ingredient_name = form.cleaned_data['ingredient_name']
+            ingredient, created = Ingredient.objects.get_or_create(name=ingredient_name)
 
+            item = form.save(commit=False)
+            item.author = request.user
+            item.save()
+            return redirect('pantry_item_detail',
+                name=item.ingredient.name,
+                quantity=item.quantity,
+                unit=item.unit,
+                location=item.storage_location.title(),
+                expiry=item.expiry_date.strftime('%Y-%m-%d') if item.expiry_date else "N/A"
+            )
+    else:
+        form = PantryForm()
+    return render(request, 'pantry/pantry_item_edit.html', {'form': form})
+
+
+# Shows saved item from URL
+def pantry_item_detail(request, name, quantity, unit, location, expiry):
+    return render(request, 'pantry/pantry_item_detail.html', {
+        'name': name,
+        'quantity': quantity,
+        'unit': unit,
+        'location': location,
+        'expiry': expiry
+    })
+
+   
 # Create your views here.
